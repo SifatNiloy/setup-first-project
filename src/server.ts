3f06058cd1a42e416a7a6bb798f589256f575aa0
SifatNiloy <sifatniloy18@gmail.com>
2024-05-26 01:26:35 +0600

    project setup Express , Mongoose, Typescript, Dotenv ,Cors

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+
+const mongoose = require('mongoose');
+
+async function main() {
+    await mongoose.connect(process.env.DATABASE_URL);
+
+}
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+  })
