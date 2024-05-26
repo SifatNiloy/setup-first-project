583000709c6846a6948b70772ae5e712954db2af
SifatNiloy <sifatniloy18@gmail.com>
2024-05-26 14:22:33 +0600

    setup done

--- dist/server.js
+++ dist/server.js
@@ -12,13 +12,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const config_1 = __importDefault(require("./app/config"));
 const app_1 = __importDefault(require("./app"));
-const mongoose = require('mongoose');
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield mongoose.connect(process.env.DATABASE_URL);
+        try {
+            yield mongoose_1.default.connect(config_1.default.database_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
     });
 }
-app_1.default.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`);
-});
+main();
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,3 @@
-// import globals from "globals";
-// import pluginJs from "@eslint/js";
-// import tseslint from "typescript-eslint";
-
-
-// export default [
-//   {
-//   languageOptions: { globals: globals.browser },
-//   ignores: ["**/node_modules/", ".dist/"],
-//   rules: {
-//     "no-unused-vars": "error",
-//     "no-undef":"error",
-//     "no-console":"warn",
-    
-//   }
-// },
-//   pluginJs.configs.recommended,
-//   ...tseslint.configs.recommended,
-// ];
 
 import globals from "globals";
 import pluginJs from "@eslint/js";
