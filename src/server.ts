536a8311ed69cd4355a1e02aed4a36d5f967f487
SifatNiloy <sifatniloy18@gmail.com>
2024-05-26 13:44:01 +0600

    basic project setup done

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import app from "./app";
-
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 
 async function main() {
-    await mongoose.connect(process.env.DATABASE_URL);
-
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-  })
+main();
